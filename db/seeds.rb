4b45c31e4ae4c9eea36608f63d612e080c7e1040
Matthew DeCarlo <matthewwho@me.com>
2019-10-27 22:32:54 -0400

    add songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# 'Baby Mine', 'Anne Walsh', 'Classic Lullabies From Around The World'
+
+songs = Song.create([{title: 'Baby Mine'}])
